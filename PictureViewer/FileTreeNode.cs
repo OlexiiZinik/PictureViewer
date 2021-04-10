e81f3c4f961ab26a419885cfecedfe8961b937f0
L3h4 <38466857+L3h4@users.noreply.github.com>
2021-04-10 03:34:18 +0300

    new featchers

---
+++ PictureViewer/FileTreeNode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureViewer
+{
+    public class FileTreeNode : TreeNode
+    {
+
+        public Picture Picture { get; set; }
+
+        
+        public FileTreeNode() : base()
+        {
+
+        }
+
+        public FileTreeNode(string text) : base(text)
+        {
+        }
+        public FileTreeNode(string text, Picture picture) : base(text)
+        {
+            Picture = picture;
+        }
+        public FileTreeNode(string text, TreeNode[] children) : base(text, children)
+        {
+        }
+
+        public FileTreeNode(string text, int imageIndex, int selectedImageIndex) : base(text, imageIndex, selectedImageIndex)
+        {
+        }
+
+        public FileTreeNode(string text, int imageIndex, int selectedImageIndex, TreeNode[] children) : base(text, imageIndex, selectedImageIndex, children)
+        {
+        }
+
+        protected FileTreeNode(SerializationInfo serializationInfo, StreamingContext context) : base(serializationInfo, context)
+        {
+        }
+    }
+}
---
+++ PictureViewer/Picture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureViewer
+{
+    public class Picture
+    {
+        public Image Image { get; set; }
+        public string Path { get; set; }
+        public string Name => Path.Split("\\".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Last();
+        public string Directory => Path.Split("\\".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Reverse().ToList()[1];
+
+        Picture()
+        { 
+        }
+
+        public Picture(string path)
+        {
+            if (!File.Exists(path))
+                throw new Exception("File not found");
+
+            Path = path;
+            Image = Image.FromFile(path);
+            
+        }
+
+
+        public static Picture FromFile(string path)
+        {
+            return new Picture(path);
+        }
+    }
+}
